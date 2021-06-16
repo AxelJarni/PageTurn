44a84371a395b034096f4d50e8794fe17237e837
Axel Jarnigon <axel.jarnigon@gmail.com>
2021-06-16 16:26:37 +0200

    Showing all books in books.php

--- books.php
+++ books.php
@@ -1,5 +1,10 @@
 <?php
-
+require "model/bookManager.php";
+$bookModel = new BookManager();
+$books = $bookModel->getBooks();
+if(!$books) {
+    $error = "Nous avons rencontré un problème, veuillez retourner à l'accueil.";
+}
 
 
 
--- view/booksView.php
+++ view/booksView.php
@@ -2,6 +2,33 @@
 include "layout/header.php";
 ?>
 
+<h3>Existing Books : </h3>
+<div class="row my-3">
+    <div>
+    <table class="table table-striped table-dark">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">Book's ID</th>
+            <th scope="col">Title</th>
+            <th scope="col">Author</th>
+            <th scope="col">Genre</th>
+            <th scope="col">Status</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach($books as $book) : ?>
+            <tr>
+            <td><?php echo $book->getID(); ?></td>
+            <td><?php echo $book->getTitle(); ?></td>
+            <td><?php echo $book->getAuthor(); ?></td>
+            <td><?php echo $book->getGenre(); ?></td>
+            <td><?php echo $book->getStatus(); ?></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+    </div>
+</div>
 
 <?php 
 include "layout/footer.php"; 
